e587689465874f8afbaa3158360e09d1c3499dd0
Marc Zych <marczych@gmail.com>
2016-03-20 17:45:26 -0700

    Solver: Add locked candidates puzzles

--- tests/solver_test.rb
+++ tests/solver_test.rb
@@ -2,6 +2,7 @@ require 'test/unit'
 require_relative '../src/sudoku/solver.rb'
 require_relative '../src/sudoku/singles_strategy.rb'
 require_relative '../src/sudoku/hidden_singles_strategy.rb'
+require_relative '../src/sudoku/locked_candidates_strategy.rb'
 
 class SolverTest < Test::Unit::TestCase
    def test_hidden_singles_puzzle
@@ -20,14 +21,22 @@ class SolverTest < Test::Unit::TestCase
       assert_puzzle('solved')
    end
 
+   def test_locked_candidates_pointing
+      assert_puzzle('locked_candidates_pointing')
+   end
+
+   def test_locked_candidates_claiming
+      assert_puzzle('locked_candidates_claiming')
+   end
+
    def assert_puzzle(puzzle_name)
       puzzle_file = "tests/puzzles/#{puzzle_name}.puz"
       solution_file = "tests/puzzles/#{puzzle_name}.sol"
       solver = Sudoku::Solver.new(File.read(puzzle_file).strip, [
          Sudoku::SinglesStrategy.new,
-         Sudoku::HiddenSinglesStrategy.new
+         Sudoku::HiddenSinglesStrategy.new,
+         Sudoku::LockedCandidatesStrategy.new,
       ])
-
 
       assert(solver.solve())
       assert_equal(File.read(solution_file).strip, solver.get_state)
